40baf74620e7bb4b66835463440d61b003771f08
Adnane Louardi <adnanelouardi-dev@gmail.com>
2021-07-21 02:28:46 +0100

    card shop

--- components/card.js
+++ components/card.js
@@ -1,5 +1,11 @@
 const Card = () => {
-   return <div className="bg-blue"></div>;
+   return (
+      <div className="bg-blue">
+         <div>
+            <h1>hello</h1>
+         </div>
+      </div>
+   );
 };
 
 export default Card;
