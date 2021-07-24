88cb2a4a671c168d8f56cea3754c37bae996baeb
Adnane Louardi <adnanelouardi-dev@gmail.com>
2021-07-24 05:26:44 +0100

    cardShop comp

--- components/card.js
+++ components/card.js
@@ -1,11 +1,11 @@
 const Card = () => {
    return (
-      <div className="p-4 my-40 h-72 w-64 mx-auto rounded-xl shadow-lg bg-gray-200">
+      <div className="p-4 my-40 w-64 mx-auto rounded-xl shadow-xl bg-gray-200">
          <div className="mx-auto h-32 rounded-xl bg-gray-400"></div>
-         <div className="mx-auto my-4 rounded-xl bg-gray-300 font-mono">
+         <div className="mx-auto mt-4 rounded-xl  font-mono">
             <div className="flex justify-between">
                <div className="mx-1">
-                  <h1 className="mt-1">Product</h1>
+                  <h1 className="mt-0.5 text-xl">Product</h1>
                </div>
 
                <div className="mx-1">
@@ -14,9 +14,14 @@ const Card = () => {
                   </svg>
                </div>
             </div>
-            <div className="my-4">
-               <div className="mx-1">
-                  <h1 className="">99.99$</h1>
+            <div className="mt-12 flex justify-between h-10 bg-yellow-500 rounded-xl overflow-hidden">
+               <div className=" text-center w-4/5">
+                  <h1 className="my-1.5 text-xl">99.99$</h1>
+               </div>
+               <div className="bg-yellow-400 w-1/5">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mx-auto my-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
                </div>
             </div>
          </div>
